969b8690a6da2162b0f44ce7afa81edc8013dce3
mamadou lamine ba <laminba2003@gmail.com>
2022-03-15 23:16:15 +0100

    improvements

--- src/main/java/com/spring/training/Application.java
+++ src/main/java/com/spring/training/Application.java
@@ -2,25 +2,12 @@ package com.spring.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/countries")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://localhost:9090/countries"))
-				.build();
 	}
 
 }
