54b1f0c3970a031236a235d51c01d47d7c6ed65b
mamadou lamine ba <laminba2003@gmail.com>
2022-12-17 18:55:29 +0000

    improvements

---
+++ src/main/java/com/spring/training/ApplicationConfig.java
@@ -0,0 +1,36 @@
+package com.spring.training;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges
+                .pathMatchers("/actuator/**").permitAll()
+                .pathMatchers("/**").authenticated()
+        ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).cors();
+        return http.build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(CorsConfiguration.ALL);
+        config.addAllowedHeader(CorsConfiguration.ALL);
+        config.addAllowedMethod(CorsConfiguration.ALL);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+
+}
