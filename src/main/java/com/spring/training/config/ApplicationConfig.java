acd34df3a1d7fc4ee873a3b9fdf52252566d8efc
mamadou lamine ba <laminba2003@gmail.com>
2023-01-10 22:10:00 +0000

    improvements

--- src/main/java/com/spring/training/config/ApplicationConfig.java
+++ src/main/java/com/spring/training/config/ApplicationConfig.java
@@ -4,13 +4,8 @@ import com.spring.training.filters.LoggingFilter;
 import com.spring.training.filters.RequestLoggingGatewayFilterFactory;
 import com.spring.training.filters.TokenLoggingGatewayFilterFactory;
 import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Configuration
 public class ApplicationConfig {
@@ -31,28 +26,16 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public List<GroupedOpenApi> apis(RouteDefinitionLocator locator, ConfigurableListableBeanFactory beanFactory) {
-        return locator.getRouteDefinitions()
-                .filter(routeDefinition -> routeDefinition.getId().matches(".*-docs"))
-                .map(routeDefinition -> {
-                    String name = routeDefinition.getId().replaceAll("-docs", "");
-                    System.out.println("name "+name);
-                    GroupedOpenApi group = groupedOpenApi(name);
-                    return group;
-                }).toStream().collect(Collectors.toList());
-    }
-
-    @Bean
     public GroupedOpenApi springConsul() {
-        return groupedOpenApi("spring-consul");
+        return createGroupedOpenApi("spring-consul");
     }
 
     @Bean
     public GroupedOpenApi springConsulClient() {
-        return groupedOpenApi("spring-consul-webclient");
+        return createGroupedOpenApi("spring-consul-webclient");
     }
 
-    public GroupedOpenApi groupedOpenApi(String name) {
+    private GroupedOpenApi createGroupedOpenApi(String name) {
         return GroupedOpenApi.builder()
                 .pathsToMatch("/" + name + "/**")
                 .group(name)
